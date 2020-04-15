0b1d58fca6da4186d6dbb42d16535578f1a4664b
Maicon Souza <maiconsouzapa@gmail.com>
2020-04-14 21:42:19 -0300

    algoritmos para teste de desempenho de Merge sort e Quick sort

--- src/ordenacao4/Desempenho.java
+++ src/ordenacao4/Desempenho.java
@@ -1,5 +1,7 @@
 package ordenacao4;
 
+import java.util.Arrays;
+
 import ordenacao4.MergeSort;
 
 public class Desempenho {
@@ -7,24 +9,30 @@ public class Desempenho {
 	 public static void main(String args[])
      {
         String saida1="\nMergeSort\n";
+        String saida2="\nArrays.sort()\n";
+        long tempoAntes, tempoDepois;
         
         for(int tam=100000; tam <= 1000000; tam=tam+100000)
         {
-           int vet1[] = new int [tam]; 
+           int vet1[] = new int [tam];
        
             //Preenchendo os vetores com elementos aleatorios de 0 a 999
            for(int i=0; i<tam; i++)
               vet1[i] = (int) (Math.random() * 1000);
            
-           long tempoAntes = System.currentTimeMillis();
+           tempoAntes = System.currentTimeMillis();
            MergeSort.mergeSort(vet1);
-           long tempoDepois = System.currentTimeMillis();
+           tempoDepois = System.currentTimeMillis();
            saida1+= "Tamanho = " + tam + "  => Tempo = " + (tempoDepois-tempoAntes) + " ms\n";
-
+           
+           tempoAntes = System.currentTimeMillis();
+           Arrays.sort(vet1);
+           tempoDepois = System.currentTimeMillis();
+           saida2+= "Tamanho = " + tam + "  => Tempo = " + (tempoDepois-tempoAntes) + " ms\n";
 
         }
 
-        System.out.println(saida1);
+        System.out.println(saida1 +saida2);
      }
 	
 }
--- src/ordenacao5/Desempenho.java
+++ src/ordenacao5/Desempenho.java
@@ -10,40 +10,37 @@ public class Desempenho {
 	 public static void main(String args[])
      {
         String saida1="\nMergeSort\n";
-        String saida2="\nQuikSort\n";
+        String saida2="\nQuickSort\n";
         String saida3="\nArraysSort\n";
+        long tempoAntes, tempoDepois;
         
         for(int tam=100000; tam <= 1000000; tam=tam+100000)
         {
-           int vet1[] = new int [tam];  
-           int vet2[] = new int [tam];
-           int vet3[] = new int [tam];
+           int vet[] = new int [tam];
        
-            //Preenchendo os vetores com elementos aleatorios de 0 a 999
+           //Preenchendo os vetores com elementos aleatorios de 0 a 999
            for(int i=0; i<tam; i++)
-              vet1[i] = vet2[i] = vet3[i]= (int) (Math.random() * 1000);
+              vet[i] = (int) (Math.random() * 1000);
            
-           long tempoAntes = System.currentTimeMillis();
-           MergeSort.mergeSort(vet1);
-           long tempoDepois = System.currentTimeMillis();
+           tempoAntes = System.currentTimeMillis();
+           MergeSort.mergeSort(vet);
+           tempoDepois = System.currentTimeMillis();
            saida1+= "Tamanho = " + tam + "  => Tempo = " + (tempoDepois-tempoAntes) + " ms\n";
            
-           long tempoAntes2 = System.currentTimeMillis();
-           Quiksort.quickSort(vet2);
-           long tempoDepois2 = System.currentTimeMillis();
-           saida2+= "Tamanho = " + tam + "  => Tempo = " + (tempoDepois2-tempoAntes2) + " ms\n";
+           tempoAntes = System.currentTimeMillis();
+           Quiksort.quickSort(vet);
+           tempoDepois = System.currentTimeMillis();
+           saida2+= "Tamanho = " + tam + "  => Tempo = " + (tempoDepois-tempoAntes) + " ms\n";
            
-           long tempoAntes3 = System.currentTimeMillis();
-           Arrays.sort(vet3);
-           long tempoDepois3 = System.currentTimeMillis();
-           saida3+= "Tamanho = " + tam + "  => Tempo = " + (tempoDepois3-tempoAntes3) + " ms\n";
+           tempoAntes = System.currentTimeMillis();
+           Arrays.sort(vet);
+           tempoDepois = System.currentTimeMillis();
+           saida3+= "Tamanho = " + tam + "  => Tempo = " + (tempoDepois-tempoAntes) + " ms\n";
 
 
         }
 
-        System.out.println(saida1);
-        System.out.println(saida2);
-        System.out.println(saida3);
+        System.out.println(saida1 +saida2 +saida3);
      }
 	
 }
