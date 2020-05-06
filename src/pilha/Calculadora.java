084e1a7d4e0f4760f3e0ddfac11934ea7a5aeac4
Davi Fonseca <davi.brandao2013@gmail.com>
2020-05-06 18:48:18 -0300

    Criação da classe que simula a calculadora hp - exercicio 3 pilha

--- src/pilha/Calculadora.java
+++ src/pilha/Calculadora.java
@@ -5,35 +5,38 @@ import java.util.Scanner;
 public class Calculadora {
 	
 	public static void main(String [] args) {
-		Pilha p1 = new Pilha(50);
-		Scanner leitura = new Scanner(System.in);
-		
-		System.out.println("Entre com (+,-,*,/): ");
-		
-		String entrada = leitura.nextLine();
-		leitura.close();
 		int numero = 0, display=0;
 		char operador =' ';
 		
-		System.out.println("Display: "+display);
+		Pilha p1 = new Pilha(50);
+		Scanner leitura = new Scanner(System.in);
+		System.out.println("Entre com um número ou um operador (+,-,*,/): ");
+		String entrada = leitura.nextLine();
+		numero = Integer.parseInt(entrada);
+		p1.empilhar(numero);
 		
-		//TODO WHILE
-		
-		try {
-			numero = Integer.parseInt(entrada);
-			p1.empilhar(numero);
-		} catch (Exception e) {
-			operador = entrada.charAt(0);
-			System.out.println("foi um operador" + operador);
-			int aux = p1.desempilhar();
-			switch(operador) {
-				case '+': display += aux; break;
-				case '-': display -= aux; break;
-				case '*': display *= aux; break;
-				case '/': display /= aux; break;
-					
+		while (p1.vazia() == false) {
+			display = numero;
+			entrada = leitura.nextLine();
+			try {
+				numero = Integer.parseInt(entrada);
+				p1.empilhar(numero);
+			} catch (Exception e) {
+				operador = entrada.charAt(0);
+				//System.out.println("Opreração:" + operador);
+				int aux = p1.desempilhar();
+				//System.out.println("Servidor:"+aux);
+				switch(operador) {
+					case '+': numero += aux; break;
+					case '-': numero -= aux; break;
+					case '*': numero *= aux; break;
+					case '/': numero /= aux; break;
+						
+				}
+				System.out.println("Display: "+numero);
 			}
 		}
+		leitura.close();
 	}
 	
 }
